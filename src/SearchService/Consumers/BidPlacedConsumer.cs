03d2b1e52f6486eb8103c13dc902e8ac3d6da3aa
Chad Eymard <eymardcm@gmail.com>
2024-08-05 16:30:02 -0400

    end of 6

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly IMapper _mapper;
+
+    public BidPlacedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed: ");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+    }
+}
